6c9969a60efb864b8765290db5f377a19d4f6ec3
dantethinker <dantethinktv@gmail.com>
2023-07-30 13:58:20 +0900

    feat: todo list props

--- mvvm/AppTodoList.js
+++ mvvm/AppTodoList.js
@@ -1,36 +1,28 @@
  app.component('app-todo-list',{
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    }
+  },
   template: /*html*/`
   <h1>Todo List</h1>
   <input v-model="newTodoText" type="text" placeholder="Enter your task...">
-  <button @click="addTodo">Add</button>
+  <button @click="$emit('addTodo', newTodoText)">Add</button>
   <ul>
     <li v-for="todo in todos" v-bind:key="todo.id">
       <input type="checkbox" v-model="todo.completed">
       <span>{{ todo.text }}</span>
-      <button @click="deleteTodo(todo.id)" class="deleteBtn">Delete</button>
+      <button 
+        @click="$emit('deleteTodo', todo.id)" 
+        class="deleteBtn"
+      >Delete</button>
     </li>
   </ul>
   `,
   data() {
       return {
-        todos: [],
         newTodoText: ''
       };
-    },
-    methods: {
-      addTodo() {
-        const todoText = this.newTodoText.trim();
-        if (todoText !== '') {
-          this.todos.push({
-            id: this.todos.length + 1,
-            text: todoText,
-            completed: false,
-          });
-          this.newTodoText = '';
-        }
-      },
-      deleteTodo(todoId) {
-        this.todos = this.todos.filter(todo => todo.id !== todoId);
-      },
     },
 })
--- mvvm/app.js
+++ mvvm/app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
   template: /*html*/`
   <div id="app">
-    <app-todo-list ></app-todo-list>
+    <app-todo-list
+      :todos="todos"
+      @addTodo="addTodo"
+      @deleteTodo="deleteTodo"
+    ></app-todo-list>
   </div>
   <div id="counter">
     <app-counter 
@@ -13,7 +17,12 @@ const app = Vue.createApp({
   `,
   data() {
     return {
-      count: 10
+      count: 10,
+      todos: [{
+        id: 1,
+        text: "exmaple text",
+        completed: true,
+      }]
     }
   },
   methods: {
@@ -22,6 +31,21 @@ const app = Vue.createApp({
     },
     minusCount(){
       this.count--;
+    },
+    addTodo(newTodoText){
+      const todoText = newTodoText.trim();
+
+      if (todoText !== '') {
+        this.todos.push({
+          id: this.todos.length + 1,
+          text: todoText,
+          completed: false,
+        });
+        this.newTodoText = '';
+      }
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
     }
   }
 })
